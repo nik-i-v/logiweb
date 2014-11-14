6e77fbdaa06fa65e18333da1330be2a42c590df9
IrinaNikulina <nik.i.v@ya.ru>
2014-11-14 14:59:14 +0300

    delete some unnecessary files

--- lw/src/main/java/ru/tsystems/javaschool/logiweb/lw/service/driver/DriverServiceForDrivers.java
+++
@@ -1,8 +0,0 @@
-package ru.tsystems.javaschool.logiweb.lw.service.driver;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface DriverServiceForDrivers {
-    void changeDriverStatusForDrivers(String license, String status);
-}
