1a15202e7307fef7c9f80a1d1ac267cc99af502a
IrinaNikulina <nik.i.v@ya.ru>
2014-11-27 00:45:44 +0300

    ui version 1.0

---
+++ lw/src/main/java/ru/tsystems/javaschool/logiweb/lw/ui/admin/DriverAction.java
@@ -0,0 +1,121 @@
+package ru.tsystems.javaschool.logiweb.lw.ui.admin;
+
+
+import ru.tsystems.javaschool.logiweb.lw.server.entities.DriverShift;
+import ru.tsystems.javaschool.logiweb.lw.server.entities.Drivers;
+import ru.tsystems.javaschool.logiweb.lw.service.admin.DriverService;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.inject.Produces;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
+
+//@Model // =@Named + @RequestScoped
+@ManagedBean(name = "driverAction")
+@ViewScoped
+/**
+ * This class provides access to services of actions with drivers company employee.
+ * The class is serializable.
+ *
+ * @author Irina Nikulina
+ */
+public class DriverAction implements Serializable {
+
+    @Inject
+    private FacesContext facesContext;
+    private static Logger logger = Logger.getLogger(DriverAction.class.getName());
+
+    private Drivers driver;
+    private List<DriverShift> drivers ;
+    private List<Long> freeDrivers;
+
+    @Produces
+    @Named
+    public List<Long> getFreeDrivers() {
+        freeDrivers = allFreeDrivers();
+        return freeDrivers;
+    }
+
+    public void setFreeDrivers(List<Long> freeDrivers) {
+        this.freeDrivers = freeDrivers;
+    }
+
+    @Produces
+    @Named
+    public Drivers getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Drivers driver) {
+        this.driver = driver;
+    }
+
+    public void setDrivers(List<DriverShift> drivers) {
+        this.drivers = drivers;
+    }
+
+    @Produces
+    @Named
+    public List<DriverShift> getDrivers(){
+        return drivers;
+    }
+    @EJB
+    private DriverService driverService;
+
+    /**
+     * Initialized a new driver and sets a value for the list of drivers.
+     */
+    @PostConstruct
+    public void initNewDriver(){
+        drivers = getAllDrivers();
+        driver = new Drivers();
+//        getAllDrivers();
+    }
+
+
+//
+    public List<DriverShift> getAllDrivers() {
+        return driverService.getAllDrivers();
+    }
+
+    /**
+     * Adds a new driver.
+     * @return true if thes driver was added and false if it's not.
+     */
+    public boolean addDriver(){
+        try {
+            driverService.addDriver(driver.getSurname(), driver.getName(), driver.getPatronymic(), driver.getLicense());
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Driver was added", "Driver addition successful"));
+            drivers = getAllDrivers();
+            driver.setLicense(null);
+            driver.setName(null);
+            driver.setSurname(null);
+            driver.setPatronymic(null);
+            return true;
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    errorMessage, "Addition unsuccessful"));
+            return false;
+        }
+    }
+
+    /**
+     * Sets a value for the list of free drivers.
+     */
+    public List<Long> allFreeDrivers(){
+        return driverService.getAllFreeDrivers();
+    }
+
+
+
+}
---
+++ lw/src/main/java/ru/tsystems/javaschool/logiweb/lw/ui/admin/OrderAction.java
@@ -0,0 +1,176 @@
+package ru.tsystems.javaschool.logiweb.lw.ui.admin;
+
+import ru.tsystems.javaschool.logiweb.lw.server.entities.Order;
+import ru.tsystems.javaschool.logiweb.lw.service.admin.OrderService;
+//import ru.tsystems.javaschool.logiweb.lw.ui.annotations.Admin;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.ejb.EJB;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * This class provides access to services of creation and closing of orders for company employee.
+ * The class is serializable.
+ *
+ * @author Irina Nikulina
+ */
+@ManagedBean(name = "orderAction")
+@ViewScoped
+public class OrderAction implements Serializable {
+    private Order order;
+    private List<Order> orders;
+    private List<Integer> createdOrderNumber;
+    private List<Integer> madeOrderNumber;
+    private List<Long> driversInCurrentOrder;
+
+    /**
+     * Returns the list of drivers in current order.
+     * @return the list of driver's licenses
+     */
+    @Named
+    @Produces
+    public List<Long> getDriversInCurrentOrder() {
+//        driversInCurrentOrder = orderService.getDriversInCurrentOrder();
+        return driversInCurrentOrder;
+    }
+
+    /**
+     * Sets drivers for certain order.
+     * @param driversInCurrentOrder the list of drivers
+     */
+    public void setDriversInCurrentOrder(List<Long> driversInCurrentOrder) {
+        this.driversInCurrentOrder = driversInCurrentOrder;
+    }
+
+    @Inject
+    private FacesContext facesContext;
+
+    @Inject
+    private Logger logger;
+
+    /**
+     * Gets numbers of orders with "made" status.
+     * @return the list of order numbers
+     */
+    @Produces
+    @Named
+    public List<Integer> getMadeOrderNumber() {
+        madeOrders();
+        return madeOrderNumber;
+    }
+
+    /**
+     * Sets numbers of orders with "made" status.
+     * @param madeOrderNumber the list of order numbers
+     */
+    public void setMadeOrderNumber(List<Integer> madeOrderNumber) {
+        this.madeOrderNumber = madeOrderNumber;
+    }
+
+    /**
+     * Gets numbers of orders with "created" status.
+     * @return the list of order numbers
+     */
+    @Produces
+    @Named
+    public List<Integer> getCreatedOrderNumber() {
+        createdOrders();
+        return createdOrderNumber;
+    }
+
+    /**
+     * Sets numbers of orders with "created" status.
+     * @param createdOrderNumber the list of order numbers
+     */
+    public void setCreatedOrderNumber(List<Integer> createdOrderNumber) {
+        this.createdOrderNumber = createdOrderNumber;
+    }
+
+    @EJB
+    private OrderService orderService;
+
+    /**
+     * Initializes orders.
+     */
+    @PostConstruct
+//    @Admin
+    public void initOrders() {
+        orders = orderService.getAllOrders();
+    }
+
+    /**
+     * Gets an order.
+     * @return
+     */
+    @Produces
+    @Named
+    public Order getOrder() {
+        return order;
+    }
+
+    /**
+     * Sets an order.
+     * @param order
+     */
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    /**
+     * Gets the list of orders.
+     * @return the list of orders.
+     */
+    @Named
+    @Produces
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+
+    /**
+     * Sets the list of orders.
+     * @param orders the list of orders.
+     */
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    /**
+     * Adds a new order.
+     */
+    public void addOrder() {
+        try {
+            orderService.addOrder();
+            logger.info("New order was created");
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Order was added", "Order addition successful"));
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    errorMessage, "Addition unsuccessful"));
+        }
+    }
+
+    /**
+     * Initializes orders with "created" status.
+     */
+    public void createdOrders() {
+        createdOrderNumber = orderService.getCreatedOrders();
+    }
+
+    /**
+     * Initializes orders with "made" status.
+     */
+    public void madeOrders() {
+        madeOrderNumber = orderService.getMadeOrders();
+    }
+
+}
